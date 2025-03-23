f5db3b384bb2a6aca39fff8ae5b38f04bd01a621
Juan Quintana <juanq79@pm.me>
2025-03-23 20:40:35 +0100

    Updated cookiecutter.json options

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -7,24 +7,34 @@ workflow_automation = '{{ cookiecutter.workflow_automation }}'
 project_report = '{{ cookiecutter.project_report }}'
 notebooks = '{{ cookiecutter.notebooks }}'
 src_structure = '{{ cookiecutter.src_structure }}'
+setup_project = "{{ cookiecutter.setup_project }}"
 
-if os_license == "No license file":
+if os_license == "Not open source":
     os.remove("LICENSE")
 
 if package_manager == "conda":
     os.remove("requirements.txt")
+    os.remove("poetry.toml")
+    os.remove("Pipfile")
 elif package_manager == "pip":
     os.remove("environment.yml")
+    os.remove("poetry.toml")
+    os.remove("Pipfile")
+elif package_manager == "poetry":
+    os.remove("requirements.txt")
+    os.remove("environment.yml")
+    os.remove("Pipfile")
+elif package_manager == "pipenv":
+    os.remove("requirements.txt")
+    os.remove("environment.yml")  
+    os.remove("poetry.toml")  
 
 if workflow_automation == "Python":
     os.remove("Makefile")
-    os.remove("Snakefile")
 elif workflow_automation == "Make":
-    os.remove("runs")
-    os.remove("Snakefile")
-elif workflow_automation == "Snakemake":
+    pass
+elif workflow_automation == "Invoke":
     os.remove("Makefile")
-    os.remove("runs")
 
 if project_report == "No":
     shutil.rmtree("reports")
@@ -37,3 +47,13 @@ if src_structure == "Less":
     #shutil.rmtree('src/features')
     shutil.rmtree('src/models')
     shutil.rmtree('src/visualization')
+
+"""
+if setup_project == "Yes - select this":
+    os.system("git init")
+    os.system("pipenv install --dev")
+    os.system(
+        "pipenv run ipython kernel install --name "
+        '"py3_{{ cookiecutter.repo_name }}" --user'
+    )
+"""
