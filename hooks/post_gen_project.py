303794748f42755c976e859d0900cd9b6a25e98c
Juan Quintana <juan.armaguedon@icloud.com>
2025-04-01 16:37:48 +0200

    Add previous .venv creation with the selected python version

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -6,6 +6,7 @@ os_license = '{{ cookiecutter.license }}'
 src_structure = '{{ cookiecutter.src_structure }}'
 project_type = '{{ cookiecutter.project_type }}'
 setup_project = "{{ cookiecutter.setup_project }}"
+python_version = "{{cookiecutter.python_version}}"
 
 if os_license == "Not open source":
     os.remove("LICENSE")
@@ -25,4 +26,5 @@ if src_structure == "Clean":
 if setup_project == "Yes":
     os.system("git init")
     os.rename(".template_gitignore", ".gitignore")
+    os.system(f"poetry env use {python_version}")
     os.system("poetry install --with dev,tests")
