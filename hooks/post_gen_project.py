978a92b34ffff989a9d2fe89c2593eadd7711a52
Juan Quintana <juan.armaguedon@icloud.com>
2025-04-01 17:02:38 +0200

    Final version

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -6,7 +6,6 @@ os_license = '{{ cookiecutter.license }}'
 src_structure = '{{ cookiecutter.src_structure }}'
 project_type = '{{ cookiecutter.project_type }}'
 setup_project = "{{ cookiecutter.setup_project }}"
-python_version = "{{cookiecutter.python_version}}"
 
 if os_license == "Not open source":
     os.remove("LICENSE")
@@ -26,5 +25,4 @@ if src_structure == "Clean":
 if setup_project == "Yes":
     os.system("git init")
     os.rename(".template_gitignore", ".gitignore")
-    os.system(f"poetry env use python{python_version}")
-    #os.system("poetry install --with dev,tests")
+    os.system("poetry install --with dev,tests")
