967d476379c6bd7428e28e731b91dd8367c35f11
Juan Quintana <juan.armaguedon@icloud.com>
2025-08-12 21:06:17 +0200

    fix(tasks): modified invoke tasks init file to allow append more than one submodule

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -2,10 +2,13 @@
 # @Author: Juan Quintana
 # @Date:   2025-08-12 18:21:40
 # @Last Modified by:   Juan Quintana
-# @Last Modified time: 2025-08-12 18:21:53
+# @Last Modified time: 2025-08-12 20:18:20
 
 from invoke import Collection
 from . import devops
+from . import test
 
 # add submodule as a collection
-ns = Collection(devops)
+ns = Collection()
+ns.add_collection(Collection(devops), name="devops")
+ns.add_collection(Collection(test), name="test")
--- {{cookiecutter.repo_name}}/tasks/__init__.py
+++ {{cookiecutter.repo_name}}/tasks/__init__.py
@@ -2,4 +2,5 @@ from invoke import Collection
 from . import devops
 
 # add submodule as a collection
-ns = Collection(devops)
+ns = Collection()
+ns.add_collection(Collection(devops), name="devops")
