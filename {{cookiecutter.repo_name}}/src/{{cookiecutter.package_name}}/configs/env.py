1181f44f976706155da38a8235dec02b8148646e
Juan Quintana <juan.armaguedon@icloud.com>
2026-04-16 12:47:10 +0200

    fix(env): fixed an issue with the way to collect automatically the folder project
    En el caso de configs.env.py, simplemente se ha mejorado para que se independiente del lugar de ejecucion dentro del proyecto, en el segundo caso, se elimino la dependencia del anterior folder project y lo que es mejor, se ha usado una herramienta propia para encontrar automaticamente el path de .env file.

--- {{cookiecutter.repo_name}}/src/{{cookiecutter.package_name}}/configs/env.py
+++ {{cookiecutter.repo_name}}/src/{{cookiecutter.package_name}}/configs/env.py
@@ -5,9 +5,16 @@ Normally data, outputs and logs folders are inside of folder project.
 However it is not true always. For this reason I give the possibility
 to define them individualy.
 """
-
 import os
 from pathlib import Path
- 
-FOLDER_PROJECT = Path(__file__).resolve().parents[3]
+
+# project folder
+FOLDER_PROJECT = Path(os.environ.get("PROJECT_ROOT", os.getcwd()))
 FOLDER_DATA = r""
+
+
+def main():
+    print(f'[info] project folder: "{FOLDER_PROJECT}"')
+
+if __name__ == "__main__":
+    main()
